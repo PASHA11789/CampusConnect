2ec3ce69c04f629daaf8b484fc0423e80cb10129
Shujaat Ali Hashim <shujaapasha11789@gmail.com>
2026-08-01 22:20:20 +0500

    fix: retry database connection gracefully instead of hard crashing nodemon on timeout

--- server/utils/db.js
+++ server/utils/db.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`DB connection sucessful`);
+    console.log(`DB connection successful: ${conn.connection.host}`);
   } catch (error) {
-    console.log("DB connection error: ", error);
-    process.exit(1);
+    console.error("DB connection error: ", error.message);
+    console.log("Retrying DB connection in 5 seconds...");
+    setTimeout(connectDB, 5000);
   }
 };
 
 export default connectDB;
+
